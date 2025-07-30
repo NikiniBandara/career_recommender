71aa67273b1cdf05c8425972d16ee1d7e3b03e43
it23148840 <it23148840@my.sliit.lk>
2025-07-30 10:19:57 +0530

    improved UI

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ st.set_page_config(page_title="Career Recommender")
 st.title("Career Recommender System")
 st.markdown("""Describe your skills, interests, and traits in your own words or select from the dropdowns to get personalized career recommendations.""")
 
+#from existing data
 skills_options, interests_options, traits_options = get_ops_from_careers()
 
+#Without this, the recommendations would disappear every time the app rerun
+if 'recommendations' not in st.session_state:
+    st.session_state.recommendations = []
+
+#get user input
 with st.form("u_input"):
     st.subheader("Your Details")
     free_text = st.text_area("Describe your skills, interests, and traits (e.g., 'I like coding, math, and being creative')", 
@@ -20,4 +26,32 @@ with st.form("u_input"):
 
     submit = st.form_submit_button("Get Recommendations")
 
-    
+    if submit:
+        if not free_text and not skills and not interests and not traits:
+            st.error("Please give at least one input")
+        else:
+            text_in = process_texts(free_text, skills_options, interests_options, traits_options)
+
+            user_in = {
+                "skills": list(set(text_in["skills"] + skills)),
+                "interests": list(set(text_in["interests"] + interests)),
+                "traits": list(set(text_in["traits"] + traits))
+            }
+
+            with st.spinner("Finding recommendations"):
+                st.session_state.recommendations = recommend_career(user_in)
+
+
+#display recommendations
+if st.session_state.recommendations:
+    st.subheader("Recommendations for you")
+    for rec in st.session_state.recommendations:
+        with st.expander(f"{rec['career']} (Score: {rec['score']})"): #a clickable dropdown box for each career recommendation
+            st.write(f"**Description**: {rec['description']}")
+            st.write(f"**Courses**: {', '.join(rec['courses'])}")
+            st.write(f"**Matched Skills**: {', '.join(rec['skills']) or 'None'}")
+            st.write(f"**Matched Interests**: {', '.join(rec['interests']) or 'None'}")
+            st.write(f"**Matched Traits**: {', '.join(rec['traits']) or 'None'}")
+        
+else:
+    st.info("No recommendations yet")
